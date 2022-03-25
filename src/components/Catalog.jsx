2aa50c511ddf402966cc0de678e052a13762e396
Your Name <bilol200414@gmail.com>
2022-03-26 02:07:13 +0500

    catalog lorem

---
+++ src/components/Catalog.jsx
@@ -0,0 +1,28 @@
+            <div className="catalog_title">
+                <div className="catalog_lorem">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis vel adipisci quasi consectetur commodi, accusantium sunt eaque praesentium fuga. Reiciendis aspernatur labore natus, nam perferendis repellendus suscipit adipisci inventore, doloribus excepturi dolor fuga vitae hic nulla! Nesciunt ipsam minima iure non adipisci, corrupti porro magnam at pariatur quis quod veniam!</p>
+                    <h2>читать полностью</h2>
+                </div>
+                <div className="catalog_icones">
+                    <div className="catalog_icone">
+                        <img src={catalogIcon} alt="" />
+                        <h4>Бесплатная доставка</h4>
+                    </div>
+
+                    <div className="catalog_icone">
+                        <img src={catalogIcon2} alt="" />
+                        <h4>Индивидуальный дизайн</h4>
+                    </div>
+                    
+                    <div className="catalog_icone">
+                        <img src={catalogIcon3} alt="" />
+                        <h4>Бесплатная примерка</h4>
+                    </div>
+
+                    <div className="catalog_icone">
+                        <img src={catalogIcon4} alt="" />
+                        <h4>Личный подход</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
