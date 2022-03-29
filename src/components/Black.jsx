74aba66680364f015eab77d695d298ea7af2a27e
Your Name <bilol200414@gmail.com>
2022-03-29 20:01:16 +0500

    black js

---
+++ src/components/Black.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "../steyles/Black.scss"
+
+
+
+function Black() {
+  return (
+  <>
+    <section className="Black">
+        <div className="container">
+                <div className="block_box">
+                    <h2>Не знаете, что выбрать?</h2>
+                    <h1 className='m'>Посетите наши салоны в Москве</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut duis tortor vitae pellentesque egestas quam pulvinar. Pellentesque porttitor velit sit pellentesque. Suspendisse donec pretium id dignissim. Dignissim ultrices eget orci viverra. Egestas quis et ut ultrices imperdiet lectus nulla tempus. Pharetra lorem sem purus nisi libero viverra ipsum.</p>
+                    <button>наши салоны</button>
+                </div>
+        </div>
+    </section>
+  </>
+  )
+}
+
+export default Black
